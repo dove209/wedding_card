213a499b94c5670166601c1d28bb4d56ce34ca69
Lee Young Jin <53247748+dove209@users.noreply.github.com>
2024-07-21 00:08:53 +0900

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from "react";
+import React, { useEffect, useState, useRef, useLayoutEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { styled } from "styled-components";
 import {
@@ -48,7 +48,7 @@ const Container = styled.div`
       margin-top: 15px;
       font-size: 12px;
       li {
-        display:flex;
+        display: flex;
         div {
           &:first-child {
             width: 70px;
@@ -69,7 +69,7 @@ const Container = styled.div`
         font-size: 10px;
         padding: 2px 5px;
         color: #fff;
-        background-color:#888;
+        background-color: #888;
       }
     }
   }
@@ -85,21 +85,20 @@ const TopTitle = styled.div`
   background: no-repeat center center url("/img/top_title.png");
   background-size: cover;
   transform: translateY(-50px);
-  /* transition: transform 1s, opacity 3s; */
-  animation: fadeInUp 2s ease forwards;
+  animation: fadeInDown 2s ease forwards;
   img {
     width: 100%;
   }
-  @keyframes fadeInUp {
+  @keyframes fadeInDown {
     from {
-        opacity: 0;
-        transform: translateY(-50px);
+      opacity: 0;
+      transform: translateY(-50px);
     }
     to {
-        opacity: 1;
-        transform: translateY(0);
+      opacity: 1;
+      transform: translateY(0);
     }
-}
+  }
 `;
 
 const MainImg = styled.div`
@@ -110,7 +109,17 @@ const MainImg = styled.div`
   background: no-repeat center center url("/img/main_img.jpg");
   background-size: cover;
   transform: translateY(50px);
-  transition: transform 1s, opacity 3s;
+  animation: fadeInUp 2s ease forwards;
+  @keyframes fadeInUp {
+    from {
+      opacity: 0;
+      transform: translateY(50px);
+    }
+    to {
+      opacity: 1;
+      transform: translateY(0);
+    }
+  }
 `;
 
 const AddressImg = styled.div`
@@ -136,7 +145,7 @@ const Gallery = styled.div`
 `;
 
 const Bottom = styled.div`
-  position:relative;
+  position: relative;
   opacity: 0;
   transform: translateY(50px);
   transition: all 1s;
@@ -148,7 +157,7 @@ const Bottom = styled.div`
     width: 50px;
     height: 50px;
   }
-`
+`;
 
 const Home = () => {
   const navigate = useNavigate();
@@ -173,43 +182,32 @@ const Home = () => {
   }, [gallayImages.length]);
 
   useEffect(() => {
-    if(mainImgRef.current && mainImgRef.current) {
-      const observer = new IntersectionObserver(
-        (entries) => {
-          entries.forEach((entry) => {
-            if (entry.isIntersecting) {
-              entry.target.classList.add("show");
-            }
-          });
-        },
-        { threshold: 0.2 }
-      );
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            entry.target.classList.add("show");
+          }
+        });
+      },
+      { threshold: 0.2 }
+    );
 
-      // observer.observe(topTitleRef.current);
-      observer.observe(mainImgRef.current);
-      observer.observe(addressImgRef.current);
-      observer.observe(gallayImgRef.current);
-      observer.observe(bottomRef.current);
-      return () => {
-        // if (topTitleRef.current) {
-        //   observer.unobserve(topTitleRef.current);
-        // }
-        if (mainImgRef.current) {
-          observer.unobserve(mainImgRef.current);
-        }
-        if (addressImgRef.current) {
-          observer.unobserve(addressImgRef.current);
-        }
-        if (gallayImgRef.current) {
-          observer.unobserve(gallayImgRef.current);
-        }
-        if(bottomRef.current) {observer.unobserve(bottomRef.current);}
-  
-      };
-    }
-    
-
-  }, [topTitleRef, mainImgRef]);
+    observer.observe(addressImgRef.current);
+    observer.observe(gallayImgRef.current);
+    observer.observe(bottomRef.current);
+    return () => {
+      if (addressImgRef.current) {
+        observer.unobserve(addressImgRef.current);
+      }
+      if (gallayImgRef.current) {
+        observer.unobserve(gallayImgRef.current);
+      }
+      if (bottomRef.current) {
+        observer.unobserve(bottomRef.current);
+      }
+    };
+  }, []);
 
   const goToLookBook = () => {
     navigate("/lookbook");
@@ -219,14 +217,42 @@ const Home = () => {
     try {
       await navigator.clipboard.writeText(text);
     } catch (e) {
-      console.log('복사에 실패하였습니다');
+      console.log("복사에 실패하였습니다");
     }
-};
+  };
 
   // 맨위로 가기 버튼 클릭
   const onClickTop = () => {
-    window.scrollTo({ top: 0, behavior: 'smooth' }); // 부드럽게 스크롤을 상단으로 이동
-  }
+    window.scrollTo({ top: 0, behavior: "smooth" }); // 부드럽게 스크롤을 상단으로 이동
+  };
+
+  useLayoutEffect(() => {
+    // 카카오 맵 API 스크립트를 동적으로 로드합니다.
+    const script = document.createElement("script");
+    script.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=ba0865e0610539648784bdb39e0ab9c5&autoload=false`;
+    script.async = true;
+    document.head.appendChild(script);
+
+    script.onload = () => {
+      window.kakao.maps.load(() => {
+        const container = document.getElementById("map"); // 'map'이라는 id를 가진 div에 맵을 그립니다.
+        const options = {
+          center: new window.kakao.maps.LatLng(37.5665, 126.978), // 초기 맵의 중심 좌표 (서울)
+          level: 3, // 초기 맵의 확대 레벨
+        };
+        const map = new window.kakao.maps.Map(container, options); // 맵을 생성합니다.
+
+        // 마커를 추가합니다.
+        const markerPosition = new window.kakao.maps.LatLng(37.5665, 126.978);
+        const marker = new window.kakao.maps.Marker({
+          position: markerPosition,
+        });
+        marker.setMap(map);
+      });
+    };
+
+    return () => script.remove(); // 컴포넌트가 언마운트될 때 스크립트를 제거합니다.
+  }, []);
 
   return (
     <Container>
@@ -373,9 +399,7 @@ const Home = () => {
         </AccordionItem>
 
         {/* 지도 */}
-        <AccordionItem
-        style={{ borderBottom: "1px solid #000",
-        }}>
+        <AccordionItem style={{ borderBottom: "1px solid #000" }}>
           {({ isExpanded }) => (
             <>
               <h2>
@@ -390,9 +414,7 @@ const Home = () => {
                       }}
                     >
                       <span>Guide</span>
-                      <span style={{ fontSize: 12 }}>
-                        오시는 길
-                      </span>
+                      <span style={{ fontSize: 12 }}>오시는 길</span>
                     </div>
                   </Box>
                   {isExpanded ? (
@@ -402,36 +424,53 @@ const Home = () => {
                   )}
                 </AccordionButton>
               </h2>
-              <AccordionPanel pb={4} style={{ borderTop: "1px solid #000", fontWeight: "bold" }}>
+              <AccordionPanel
+                pb={4}
+                style={{ borderTop: "1px solid #000", fontWeight: "bold" }}
+              >
                 <h1 style={{ fontSize: 14 }}>Guide</h1>
                 <p style={{ fontSize: 13, marginTop: 15 }}>
-                  2024년 8월 31일 토요일 오후 3시<br />
-                  세빛섬 플로팅아일랜드 2F 컨벤션홀<br />
-                  서울 서초구 올림픽대로 2085-14<br />
+                  2024년 8월 31일 토요일 오후 3시
+                  <br />
+                  세빛섬 플로팅아일랜드 2F 컨벤션홀
+                  <br />
+                  서울 서초구 올림픽대로 2085-14
+                  <br />
                   TEL 02-537-2440
                   <br />
                   <br />
-                  - 지하철&nbsp;&nbsp;고속터미널 6번출구 하차 후 셔틀 이용<br />
-                  - 버스&nbsp;&nbsp;반포한공.세빛섬 하차<br />
-                  - 주차&nbsp;&nbsp;세빛섬 주차장
+                  - 지하철&nbsp;&nbsp;고속터미널 6번출구 하차 후 셔틀 이용
+                  <br />
+                  - 버스&nbsp;&nbsp;반포한공.세빛섬 하차
+                  <br />- 주차&nbsp;&nbsp;세빛섬 주차장
                 </p>
+
+                <div
+                  id="map"
+                  style={{
+                    width: "80%",
+                    height: "150px",
+                    marginTop: 40,
+                    marginBottom: 40,
+                  }}
+                ></div>
               </AccordionPanel>
             </>
           )}
         </AccordionItem>
 
-
-        
         {/* 계좌  */}
-        <AccordionItem          style={{
+        <AccordionItem
+          style={{
             borderBottom: "1px solid #000",
-          }}>
+          }}
+        >
           {({ isExpanded }) => (
             <>
               <h2>
                 <AccordionButton>
                   <Box as="span" flex="1" textAlign="left">
-                  <div
+                    <div
                       style={{
                         display: "flex",
                         justifyContent: "space-between",
@@ -440,9 +479,7 @@ const Home = () => {
                       }}
                     >
                       <span>Info</span>
-                      <span style={{ fontSize: 12 }}>
-                        마음 전하실 곳
-                      </span>
+                      <span style={{ fontSize: 12 }}>마음 전하실 곳</span>
                     </div>
                   </Box>
                   {isExpanded ? (
@@ -452,13 +489,23 @@ const Home = () => {
                   )}
                 </AccordionButton>
               </h2>
-              <AccordionPanel pb={4} style={{ borderTop: "1px solid #000", fontWeight: "bold" }}>
+              <AccordionPanel
+                pb={4}
+                style={{ borderTop: "1px solid #000", fontWeight: "bold" }}
+              >
                 <h1 style={{ fontSize: 14 }}>info</h1>
                 <ul className="ul2">
                   <li>
                     <div>
                       <p>신랑 계좌</p>
-                      <p className="copy_btn" onClick={() => {handleCopyClipBoard('9003-2517-3390-6')}}>복사하기</p>
+                      <p
+                        className="copy_btn"
+                        onClick={() => {
+                          handleCopyClipBoard("9003-2517-3390-6");
+                        }}
+                      >
+                        복사하기
+                      </p>
                     </div>
                     <div>
                       <p>새마을 (예금주: 방재혁)</p>
@@ -468,7 +515,14 @@ const Home = () => {
                   <li>
                     <div>
                       <p>신랑혼주 계좌</p>
-                      <p className="copy_btn" onClick={() => {handleCopyClipBoard('1183-12-007811')}}>복사하기</p>
+                      <p
+                        className="copy_btn"
+                        onClick={() => {
+                          handleCopyClipBoard("1183-12-007811");
+                        }}
+                      >
+                        복사하기
+                      </p>
                     </div>
                     <div>
                       <p>농협 (예금주: 방극만)</p>
@@ -478,7 +532,14 @@ const Home = () => {
                   <li>
                     <div>
                       <p>신부 계좌</p>
-                      <p className="copy_btn" onClick={() => {handleCopyClipBoard('110-402-192440')}}>복사하기</p>
+                      <p
+                        className="copy_btn"
+                        onClick={() => {
+                          handleCopyClipBoard("110-402-192440");
+                        }}
+                      >
+                        복사하기
+                      </p>
                     </div>
                     <div>
                       <p>신한 (예금주: 오하늬)</p>
@@ -488,7 +549,14 @@ const Home = () => {
                   <li>
                     <div>
                       <p>신부혼주 계좌</p>
-                      <p className="copy_btn" onClick={() => {handleCopyClipBoard('064602-04-075569')}}>복사하기</p>
+                      <p
+                        className="copy_btn"
+                        onClick={() => {
+                          handleCopyClipBoard("064602-04-075569");
+                        }}
+                      >
+                        복사하기
+                      </p>
                     </div>
                     <div>
                       <p>국민 (예금주: 오세연)</p>
@@ -507,8 +575,18 @@ const Home = () => {
       </Accordion>
 
       <Bottom ref={bottomRef}>
-        <img className='not_click' style={{width: '100%'}} src='/img/lookbook/bottom.png'  alt='bottom 이미지'/>
-        <img className='not_click top' src='/img/lookbook/top.png'  alt='top 이미지' onClick={onClickTop}  />
+        <img
+          className="not_click"
+          style={{ width: "100%" }}
+          src="/img/lookbook/bottom.png"
+          alt="bottom 이미지"
+        />
+        <img
+          className="not_click top"
+          src="/img/lookbook/top.png"
+          alt="top 이미지"
+          onClick={onClickTop}
+        />
       </Bottom>
     </Container>
   );
